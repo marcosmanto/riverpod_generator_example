7ec5fc182688d49b5ab26bb14bcb4a65fbe098b5
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-11-29 21:32:11 -0300

    Infinite scrolling implemented without a limit (last page).

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -17,6 +19,7 @@ class MainApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
+      scrollBehavior: MyCustomScrollBehavior(),
       home: Home(),
     );
   }
@@ -37,7 +40,31 @@ class Home extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Infinite scrolling'),
       ),
-      body: Container(),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final page = index ~/ 50;
+          final itemIndexOnPage = index % 50;
+          final pageValue = ref.watch(fetchItemsProvider(page: page));
+
+          return pageValue.when(
+            data: (items) {
+              return Text(items[itemIndexOnPage]);
+            },
+            error: (error, stackTrace) => Text('err $error'),
+            loading: () => Text('Loading'),
+          );
+
+          return Text('$index');
+        },
+      ),
     );
   }
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
 }
